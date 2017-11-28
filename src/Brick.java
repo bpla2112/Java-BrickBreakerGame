226e4e79a06b9aeeda82f48a27779713fb86eb92
Bernardo Pla <bpla2112@gmail.com>
2017-11-28 16:55:37 -0500

    Java Brick Breaker game has been completed per project course. There is some stuff that can still be done but for another time.

--- src/Brick.java
+++ src/Brick.java
@@ -4,13 +4,17 @@ public class Brick extends Rectangle{
     Image picture;
     int dx = 3;
     int dy = -3;
+    Rectangle left, right;
     boolean destroyed = false;
+    boolean powerup = false;
 
     Brick(int a, int b, int w, int h, String s){
         x = a;
         y = b;
         width = w;
         height = h;
+        left = new Rectangle(a-1, b, 1, h);
+        right = new Rectangle(a+w+1, b, 1, h);
         picture = Toolkit.getDefaultToolkit().getImage(s);
     }
 
--- src/BrickBreakerPanel.java
+++ src/BrickBreakerPanel.java
@@ -3,12 +3,14 @@ import java.awt.*;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.util.ArrayList;
+import java.util.Random;
 
 public class BrickBreakerPanel extends JPanel implements KeyListener {
 
     //create arraylist of objects
     ArrayList<Brick> bricks = new ArrayList<Brick>();
     ArrayList<Brick> ball = new ArrayList<Brick>();
+    ArrayList<Brick> powerup = new ArrayList<Brick>();
     Brick paddle;
     Thread thread;
     Animate animate;
@@ -28,6 +30,13 @@ public class BrickBreakerPanel extends JPanel implements KeyListener {
         for(int i = 0; i < 8; i++){
             bricks.add(new Brick((i * 60 + 2), 75, 60, 25, "yellow.png"));
         }
+        Random random = new Random();
+        bricks.get(random.nextInt(32)).powerup = true;
+        bricks.get(random.nextInt(32)).powerup = true;
+        bricks.get(random.nextInt(32)).powerup = true;
+        bricks.get(random.nextInt(32)).powerup = true;
+        bricks.get(random.nextInt(32)).powerup = true;
+        bricks.get(random.nextInt(32)).powerup = true;
         ball.add(new Brick(237, 437, 25, 25, "ball.png"));
         addKeyListener(this);
         setFocusable(true);
@@ -42,10 +51,21 @@ public class BrickBreakerPanel extends JPanel implements KeyListener {
         for(Brick b : ball){
             b.draw(g, this);
         }
+        for(Brick p : powerup){
+            p.draw(g, this);
+        }
         paddle.draw(g, this);
     }
 
     public void update(){
+        for(Brick p : powerup){
+            p.y += 1; //drop the powerup
+
+            if(p.intersects(paddle) && !p.destroyed){
+                p.destroyed = true;
+                ball.add(new Brick(paddle.dx + 75, 437, 25, 25, "ball.png"));
+            }
+        }
         for(Brick ba: ball){
             ba.x += ba.dx;
             if(ba.x > (getWidth() - size) && ba.x > 0 || ba.x < 0){
@@ -57,9 +77,19 @@ public class BrickBreakerPanel extends JPanel implements KeyListener {
             ba.y += ba.dy;
 
             for(Brick b : bricks){
-                if(ba.intersects(b) && !b.destroyed){
+                if((b.left.intersects(ba) || b.right.intersects(ba)) && !b.destroyed){
+                    ba.dx *= -1;
+                    b.destroyed = true;
+                    if(b.powerup){
+                        powerup.add(new Brick(b.x, b.y, 25, 19, "extra.png"));
+                    }
+                }
+                else if(ba.intersects(b) && !b.destroyed){
                     b.destroyed = true;
                     ba.dy *= -1;
+                    if(b.powerup){
+                        powerup.add(new Brick(b.x, b.y, 25, 19, "extra.png"));
+                    }
                 }
             }
         }
